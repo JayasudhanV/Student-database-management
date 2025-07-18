be0571e198226bd7d725e8d37237f0f7244e9e4a
JayasudhanV <230701131@rajalakshmi.edu.in>
2025-07-18 22:05:06 +0530

    Add files via upload

---
+++ Studentdatabasemanagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentdatabasemanagement;
+
+/**
+ *
+ * @author jayas
+ */
+public class Studentdatabasemanagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
